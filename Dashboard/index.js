9479b722da8e472a36fcf8fc2ce0bcce430e6e23
biteeunkilllable <biteeunkillable@gmail.com>
2023-08-29 15:43:47 +0300

    patch$16

---
+++ Dashboard/index.js
@@ -0,0 +1,16 @@
+function carding(username, promise) {
+  return `<div class="card">
+            <div class="card2">
+              <h1>${username} : </h1>
+              <br/>${promise}
+              </div>
+          </div>`;
+}
+fetch("https://boardofhopes.somee.com/api/PromiseHandler/GetPromsies")
+  .then((res) => res.json())
+  .then((cards) => {
+    let container = document.getElementById("warpper");
+    for (let card of cards) {
+      container.prepend(carding(card.Name, card.Promise));
+    }
+  });
